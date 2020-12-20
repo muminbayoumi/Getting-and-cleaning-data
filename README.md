2c204bf0e6b914bcc8a2a880d90ab03c46137555
Mumin Bayoumi <muminbayoumi@gmail.com>
2020-12-20 02:28:56 +0000

    Comit includes  complete run_analysis file and  2 required RMDs

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# Getting-and-cleaning-data
+### Final Assignment QUIZ 4
+
+This readme  accompanies and  clarifies an already annotated  run_analsysis .R  file. 
+
+In order to comfortabley do this  assingment  I had to depart from the provided  step order 
+with an  identical final result. 
+
+I created a  Step 0 - 
+This  focuses on reading in the  data
+It seems the data is in 4 seperate files that had to be smashed together somehow. 
+4 seperate files for each of the train and test data sets. 
+I had to duplicate the process for train and test datasets  then merge them. I imagine
+there will be a simple way of  lapplying or forlooping this but for sake of simplicity 
+and clarity i seperated the 2. 
+
+Hear is an explanation the variables i read in and how i labelled them: 
+
+**Activity.labels** - This a reference  used later to replace numbers with their corresponding activities.(1=Walking ...etc)
+
+**feature.names** - This is a reference used later to replace V1..etc with a meaningful corresponding variable names. Variables
+are essentially measurement from different sensors. 
+
+**test.x** and **train.x** are the  actual measurements - not yet labelled by corresponding  subjects and activities
+**train**/**test** **.act.code** & **train**/**test** **.subject.code** where used to labell the  previous mentioned test.x and train.x 
+with subjects and  activities.
+
+
+essentially by binding **train.x** + **train.act.code** + **train.subject.code** columnwise and  then repeating for the  test dataset i got 2  datasets 
+
+
+
+
+
+###### **STEP 1 - merge Train and Test Datasets**
+which i then joined  by row to form the  **full** dataset
+
+###### **STEP 3&4 -**
+###### **-Appropriately Name data set columns with meaningful names(instead of  V1..etc) & **
+###### **-Use descriptive activity names to name the activities in the data set (1=Walking .. etc)**
+This  step made more sense to add this stage since i will be suing those to extraxt  specific columns
+I did this by using the the provided  feature names and acivity labels.
+
+
+###### **STEP 2 Extracting only the measurements on the mean and standard deviation for each measurement.**
+For this i used **dplyr** *select* function with the  *contains()*  option to select only  variables(columns) which contain
+'mean' or 'std' and assigned  it to **SubsetData**
+
+In **SubsetData** - each row is a seperate set of  measurments of  a subject  during an activity . It seems there is  more than one set of measurement for each subject during each activity which is  why it makes sense to summarise in the next  step using the  mean function.(**Tidy  Dataset!**)
+
+###### **STEP 5 **
+Again Used  **Dplyr** to group  the  data set by Activity then by Subject to Summarise the  different repeated measurements to get
+their corresponding means. This  was  done using the *summarize* functions using the *across*  option.
+Finally - USed **Janitor**  *clean_names* to clean up  the variable names. Which will be further  explained in the codebook .rmd file.
+
+
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,97 @@
+if(!file.exists("data.zip")) {
+        download.file(
+                "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+                destfile = "data.zip",
+                method = "curl"
+        )
+}
+if (!file.exists("UCI HAR Dataset")) {
+        unzip("data.zip", overwrite = T, exdir = "../Quiz4/")
+}
+
+library(tidyverse)
+library(magrittr)
+library(data.table)
+### STEP 0
+### This initial step (not a formal quiz step)is about reading in all the files that will be needed for
+### further steps
+
+## Reading Acitvity labels
+activity.labels<- fread("UCI HAR Dataset/activity_labels.txt")
+## Reading Meaninful Feature Names
+feature.names <- fread(input = "UCI HAR Dataset/features.txt")
+
+
+
+## Reading Train Dataset
+        train.x <- fread("UCI HAR Dataset/train/X_train.txt")
+        # Checking no. of columns on train data set matches number number of feature names
+        # this makes it  future proof if  more features are added
+        if (length(feature.names$V1) != length(colnames(train.x)))  {
+        warning("No. of feature names loaded doesnt match no. of columns on dataset")
+        }
+        # Loading corresponding activity codes for train data set
+        train.act.code <- fread(input = "UCI HAR Dataset/train/y_train.txt")
+        #Loading Corresponding Subject Dataset
+        train.subject.code <-fread(input = "UCI HAR Dataset/train/subject_train.txt")
+        #Adding activity code and subject code to data set
+        train.x %<>%
+        #and creating another column called Train to maintain the information that this a train dataset on binding
+        mutate(Activity = train.act.code$V1,
+               Subject = train.subject.code$V1,
+               Train = TRUE) %>%
+        #this step bring last two columns to the beginnig of dataset for readablity
+        select(Activity, Train, Subject, V1:V561)
+
+
+## Reading Test Dataset
+        test.x <- fread("UCI HAR Dataset/test/X_test.txt")
+        # Checking no. of columns on test data set matches number number of feature names
+        # Again to make future proof
+        if (length(feature.names$V1) != length(colnames(test.x)))  {
+        warning("No. of feature names loaded doesnt match no. of columns on dataset")
+        }
+        # Loading corresponding activity codes  and subject codes for test data set
+        test.act.code <-  fread("UCI HAR Dataset/test/y_test.txt")
+        test.subject.code <-fread(input = "UCI HAR Dataset/test/subject_test.txt")
+
+
+        #Adding activity code and subject code to data set
+        test.x %<>%
+        #and creating another column called Train to maintain information on binding
+        mutate(Activity = test.act.code$V1,
+               Subject = test.subject.code$V1,
+               Train = FALSE) %>%
+        #this step brings last two columns to begging of data set for readablity
+        select(Activity, Train, Subject, V1:V561)
+
+
+
+
+## Merging the training and the test sets to create one data set. (STEP 1)
+        full <- rbind(train.x, test.x)
+
+## Giving Meaningful names to variables using feature names file (STEP 4)
+        colnames(full)[4:564] <- make.names(feature.names$V2
+                                    # this makes some duplicate column names unique to avoid error with dplyr
+                                    , unique = T)
+        #Setting activity column to categorical variable with levels corresponding to activity.labels  (STEP 3 - merged data set)
+        full$Activity %<>% factor(level = activity.labels$V1,
+                          label = activity.labels$V2,
+                          ordered = F)
+
+## Extracting mean and sd only from available variables (STEP 2)
+        subsetData <-  full %>% select(Activity, Train,Subject, contains("mean."), contains("std."))
+
+
+## Summarising Data per subject and activity and making column names clean
+library(janitor)#used  for the  clean_names  function
+
+## Using dplyr to group by activity then by subject and then summarising all
+## columns  by taking means of respective groups
+SummarisedSubset <- subsetData %>%
+        group_by(Activity,Subject) %>%
+        summarise(across(.col=everything(),.fns=mean)) %>% clean_names(case='small_camel')
+
+SummarisedSubset %>% select(1:3,order(colnames(SummarisedSubset)))
+
